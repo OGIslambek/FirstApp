8a9da011aa1663db126e344c05bc91968d9b25c4
OGislambek <ogislambek@gmail.com>
2024-08-19 23:59:39 +0300

    Ура, я изменил значение переменной myName

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myName;
+            myName = "Mary";
+
+            Console.WriteLine(myName);
+
+            Console.ReadKey();
         }
     }
 }
