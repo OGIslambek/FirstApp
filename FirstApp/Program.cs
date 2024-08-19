e555b855b85a613e71eb90c47f6cb376e7801ce8
OGislambek <ogislambek@gmail.com>
2024-08-20 00:46:52 +0300

    Заполнил анкету

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -6,8 +6,10 @@ namespace FirstApp
     {
         static void Main(string[] args)
         {
-            string myName;
-            myName = "Mary";
+            string myName = "Mary";
+            byte age = 27;
+            bool haveAPet = true;
+            double shoeSize = 37.5;
 
             Console.WriteLine(myName);
 
@@ -15,8 +17,13 @@ namespace FirstApp
             Console.WriteLine("\t Мне 27 лет");
             Console.WriteLine($"\t My name is \n {myName}");
             Console.WriteLine("\u0040");
-
             Console.WriteLine("\x23");
+
+            Console.WriteLine("Как вас зовут?\nСколько вам лет?\nЕсли ли у вас питомец?\nКакой у вас размер ноги?\n");
+            Console.WriteLine($"Меня зовут {myName}");
+            Console.WriteLine($"Мне {age} лет");
+            Console.WriteLine($"У меня есть питомец? {haveAPet}");
+            Console.WriteLine($"Мой размер ноги {shoeSize}");
 
             Console.ReadKey();
         }
