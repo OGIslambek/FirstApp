278f60a7288134e6c08e6baa792e69e1758ab23e
OGislambek <ogislambek@gmail.com>
2024-08-20 00:23:32 +0300

    Отобразил знак @ Добавил вывод символа #

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace FirstApp
+﻿using System.Xml.Linq;
+
+namespace FirstApp
 {
     internal class Program
     {
@@ -8,6 +10,13 @@
             myName = "Mary";
 
             Console.WriteLine(myName);
+
+            Console.WriteLine("\t Привет Мир");
+            Console.WriteLine("\t Мне 27 лет");
+            Console.WriteLine($"\t My name is \n {myName}");
+            Console.WriteLine("\u0040");
+
+            Console.WriteLine("\x23");
 
             Console.ReadKey();
         }
